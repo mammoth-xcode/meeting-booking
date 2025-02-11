8e844b1e8774ba87f0d6fb0f0c201da6b2570235
mammoth-xcode <mammoth.xcode@gmail.com>
2025-02-11 15:44:33 +0700

    Update AddRoomModal.jsx

--- src/app/components/AddRoomModal.jsx
+++ src/app/components/AddRoomModal.jsx
@@ -191,7 +191,7 @@ const AddRoomModal = ({ isOpen, onClose, onSave, roomtypes, equipments }) => {
 
         // local
         // "/api/upload_local"
-        const response2 = await fetch('/api/upload_local', {
+        const response2 = await fetch('/api/upload', {
           method: 'POST',
           body: formData2,
         });
